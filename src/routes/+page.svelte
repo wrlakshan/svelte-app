74e1ba18eae3cb08b13140fba9eaec7a30e70bb0
wrlakshan <rashmika.lakshan@ebuilder.com>
2024-06-26 13:01:55 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	let name = 'Rashmika';
+	let num = 1;
+	const handleClick = () => {
+		num = num + 1;
+	};
+</script>
+
+<main>
+	<h1>Welcome to {name} {num}</h1>
+	<button on:click={handleClick}>update the color</button>
+	<input type="text" bind:value={name} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+	}
+
+	h1 {
+		font-size: 2rem;
+		color: #178882;
+		text-align: center;
+		margin-top: 4em;
+	}
+</style>
